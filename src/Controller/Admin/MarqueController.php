e6f79e741f50a1f01abb14feb8ada73d211f19a5
Nicolas <reitin.pro@gmail.com>
2024-07-19 09:05:26 +0200

    CRUD Done

--- src/Controller/Admin/MarqueController.php
+++ src/Controller/Admin/MarqueController.php
@@ -2,10 +2,14 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Marque;
+use App\Form\MarqueType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 use App\Repository\MarqueRepository;
+use Doctrine\ORM\EntityManagerInterface;
 
 class MarqueController extends AbstractController
 {
@@ -20,4 +24,63 @@ class MarqueController extends AbstractController
             'marques' => $marques,
         ]);
     }
+
+
+    /**
+     * @Route("/admin/marques/new", name="admin_marque_new", methods={"GET", "POST"})
+     */
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $marque = new Marque();
+        $form = $this->createForm(MarqueType::class, $marque);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $marque->setCreatedAt(new \DateTimeImmutable());
+            $entityManager->persist($marque);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('admin_marque_index');
+        }
+
+        return $this->render('@SyliusAdmin/Marque/Grid/new.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+
+    /**
+     * @Route("/admin/marques/edit/{id}", name="admin_marque_edit", methods={"GET", "POST"})
+     */
+    public function edit(Request $request, Marque $marque, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(MarqueType::class, $marque);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $marque->setUpdatedAt(new \DateTimeImmutable());
+            $entityManager->flush();
+
+            return $this->redirectToRoute('admin_marque_index');
+        }
+
+        return $this->render('@SyliusAdmin/Marque/Grid/edit.html.twig', [
+            'form' => $form->createView(),
+            'marque' => $marque,
+        ]);
+    }
+
+    /**
+     * @Route("/admin/marques/delete/{id}", name="admin_marque_delete", methods={"POST"})
+     */
+    public function delete(Request $request, Marque $marque, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$marque->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($marque);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('admin_marque_index');
+    }
+
 }
---
+++ src/Form/MarqueType.php
@@ -0,0 +1,33 @@
+<?php 
+
+namespace App\Form;
+
+use App\Entity\Marque;
+use DateTime;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MarqueType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'label' => 'Nom',
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => 'Description',
+                'required' => false,
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Marque::class,
+        ]);
+    }
+}
